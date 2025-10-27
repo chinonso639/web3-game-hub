abd99707a3d2ebebbac0274ec15581bb65d2527b
chinonso <nonc639@gmail.com>
2025-10-27 18:39:05 +0100

    update 1.6 testing the chess noard centring

--- app/chess/page.tsx
+++ app/chess/page.tsx
@@ -5,7 +5,8 @@ import { ChessBoard } from "@/components/chess/chess-board";
 export default function ChessPage() {
   return (
     <div className="container mx-auto p-4">
-      <div className="max-w-4xl mx-auto">
+      {/* On mobile, vertically and horizontally center the board */}
+      <div className="max-w-4xl mx-auto flex sm:block items-center justify-center min-h-[100svh] sm:min-h-0">
         <ChessBoard />
       </div>
     </div>
